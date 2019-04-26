89aa2810d9b0ee155ec0e2d2bdea383f376ee207
David <dhahn123@gmail.com>
2019-04-26 00:47:53 -0700

    set up proxy, build script to ../public, set up server.js with path

---
+++ public/precache-manifest.d76917399cce7bab147dda8c50f79910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af69d0374944ce33879b44db91116d7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc930a913e8c685f01ee",
+    "url": "/static/css/main.e23fee82.chunk.css"
+  },
+  {
+    "revision": "3243b53e4eaa78bf0752",
+    "url": "/static/js/2.8ac3516b.chunk.js"
+  },
+  {
+    "revision": "cc930a913e8c685f01ee",
+    "url": "/static/js/main.ce402232.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b15ef94746031f8853af907909881f2a",
+    "url": "/static/media/logo.b15ef947.jpg"
+  }
+]);
